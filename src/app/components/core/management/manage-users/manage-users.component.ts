d972fcc6df929a765a4b9cf252ab7fde9e2d36f3
Mario Ehab <mario-ehab@spatium.com>
2024-02-25 09:50:51 +0200

    Responsive header

--- src/app/components/core/management/manage-users/manage-users.component.ts
+++ src/app/components/core/management/manage-users/manage-users.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CardComponent } from '../../../../shared/card/card.component';
 import { userManagementCard } from '../../../../../stories/card.stories';
+import { TableComponent } from '../../../../shared/table/table.component';
 import { BreadcrumbService } from '../../../../services/breadcrumb/breadcrumb.service';
 
 @Component({
   selector: 'app-manage-users',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent,TableComponent],
   templateUrl: './manage-users.component.html',
   styleUrl: './manage-users.component.css'
 })
-export class ManageUsersComponent implements OnInit {
+export class ManageUsersComponent {
+  row1=1;
+  row2 = 2;
+  headdata = [this.row1,this.row2];
+  bodydata = [1,2,3,4,5];
+  
   constructor(private _BreadCurmb: BreadcrumbService) { }
 
   ngOnInit() {
