3ba9be856e6a3cefd69742371be10942927ed62b
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2024-03-05 15:09:22 +0200

    create the table component

--- src/app/shared/table/table.component.ts
+++ src/app/shared/table/table.component.ts
@@ -8,9 +8,17 @@ import { Component, Input } from '@angular/core';
   styleUrl: './table.component.css'
 })
 export class TableComponent {
-  @Input() headdata:any = [];
-  @Input() bodydata:any = [];
-  constructor(){}
+  headdata:string[] = ['User ID',"USer info","Status","Role name","Email","Joind at"];
+  bodydata:any = [
+    {id:'#U-101',info:{path:'../../../assets/images/table-demo-image.jpg',name:'Will Smith'},status:'Active',role:'Super admin',email:'mahmoudsmohammed24@gmail.com',date:'11:43 am-18jun,2024'},
+  {id:'#U-101',info:{path:'../../../assets/images/table-demo-image.jpg',name:'Will Smith'},status:'Deactive',role:'Super admin',email:'mahmoudsmohammed24@gmail.com',date:'11:43 am-18jun,2024'},
+  {id:'#U-101',info:{path:'../../../assets/images/table-demo-image.jpg',name:'Will Smith'},status:'Pending',role:'Super admin',email:'mahmoudsmohammed24@gmail.com',date:'11:43 am-18jun,2024'},{id:'#U-101',info:{path:'../../../assets/images/table-demo-image.jpg',name:'Will Smith'},status:'Active',role:'Super admin',email:'mahmoudsmohammed24@gmail.com',date:'11:43 am-18jun,2024'},
+  {id:'#U-101',info:{path:'../../../assets/images/table-demo-image.jpg',name:'Will Smith'},status:'Deactive',role:'Super admin',email:'mahmoudsmohammed24@gmail.com',date:'11:43 am-18jun,2024'},
+  {id:'#U-101',info:{path:'../../../assets/images/table-demo-image.jpg',name:'Will Smith'},status:'Pending',role:'Super admin',email:'mahmoudsmohammed24@gmail.com',date:'11:43 am-18jun,2024'}];
+  
+  constructor(){
+    console.log(this.bodydata[0].id)
+  }
 
 
 }
