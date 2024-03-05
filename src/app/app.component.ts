9455376cf662828ceec9479dac1d95a8bf537a0e
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2024-03-05 11:57:03 +0200

    paginator responsive design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { SignupComponent } from './components/Authentication/signup/signup.component';
 
 import { ReactiveFormsModule } from '@angular/forms';
-import { PaginatorComponent } from './shared/paginator/paginator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SignupComponent, ReactiveFormsModule,PaginatorComponent],
+  imports: [RouterOutlet, SignupComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
