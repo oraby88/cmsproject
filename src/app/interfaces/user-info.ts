44d280c65f663b1095bf0e3f2d035f716331d7fc
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2024-03-06 12:11:47 +0200

    integration on table component

---
+++ src/app/interfaces/user-info.ts
@@ -0,0 +1,9 @@
+export interface userInfo {
+  id: string
+  fullName: string
+  profileImagePath: string
+  isAccountActive: boolean
+  email: string
+  createdAt: string
+  roleName: string
+}
--- src/app/shared/table/table.component.ts
+++ src/app/shared/table/table.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PaginatorComponent } from '../paginator/paginator.component';
+import { userInfo } from '../../interfaces/user-info';
+import { UserManagementsService } from '../../services/user-managements/user-managements.service';
 
 @Component({
   selector: 'app-table',
@@ -8,17 +10,24 @@ import { PaginatorComponent } from '../paginator/paginator.component';
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
-export class TableComponent {
+export class TableComponent implements OnInit{
   headdata:string[] = ['User ID',"USer info","Status","Role name","Email","Joind at"];
-  bodydata:any = [
-    {id:'#U-101',info:{path:'../../../assets/images/table-demo-image.jpg',name:'Will Smith'},status:'Active',role:'Super admin',email:'mahmoudsmohammed24@gmail.com',date:'11:43 am-18jun,2024'},
-  {id:'#U-101',info:{path:'../../../assets/images/table-demo-image.jpg',name:'Will Smith'},status:'Deactive',role:'Super admin',email:'mahmoudsmohammed24@gmail.com',date:'11:43 am-18jun,2024'},
-  {id:'#U-101',info:{path:'../../../assets/images/table-demo-image.jpg',name:'Will Smith'},status:'Pending',role:'Super admin',email:'mahmoudsmohammed24@gmail.com',date:'11:43 am-18jun,2024'},{id:'#U-101',info:{path:'../../../assets/images/table-demo-image.jpg',name:'Will Smith'},status:'Active',role:'Super admin',email:'mahmoudsmohammed24@gmail.com',date:'11:43 am-18jun,2024'},
-  {id:'#U-101',info:{path:'../../../assets/images/table-demo-image.jpg',name:'Will Smith'},status:'Deactive',role:'Super admin',email:'mahmoudsmohammed24@gmail.com',date:'11:43 am-18jun,2024'},
-  {id:'#U-101',info:{path:'../../../assets/images/table-demo-image.jpg',name:'Will Smith'},status:'Pending',role:'Super admin',email:'mahmoudsmohammed24@gmail.com',date:'11:43 am-18jun,2024'}];
+  bodydata!: userInfo[];
+  // bodydata:any = [
+  //   {id:'#U-101',info:{path:'../../../assets/images/table-demo-image.jpg',name:'Will Smith'},status:'Active',role:'Super admin',email:'mahmoudsmohammed24@gmail.com',date:'11:43 am-18jun,2024'},
+  // {id:'#U-101',info:{path:'../../../assets/images/table-demo-image.jpg',name:'Will Smith'},status:'Deactive',role:'Super admin',email:'mahmoudsmohammed24@gmail.com',date:'11:43 am-18jun,2024'},
+  // {id:'#U-101',info:{path:'../../../assets/images/table-demo-image.jpg',name:'Will Smith'},status:'Pending',role:'Super admin',email:'mahmoudsmohammed24@gmail.com',date:'11:43 am-18jun,2024'},{id:'#U-101',info:{path:'../../../assets/images/table-demo-image.jpg',name:'Will Smith'},status:'Active',role:'Super admin',email:'mahmoudsmohammed24@gmail.com',date:'11:43 am-18jun,2024'},
+  // {id:'#U-101',info:{path:'../../../assets/images/table-demo-image.jpg',name:'Will Smith'},status:'Deactive',role:'Super admin',email:'mahmoudsmohammed24@gmail.com',date:'11:43 am-18jun,2024'},
+  // {id:'#U-101',info:{path:'../../../assets/images/table-demo-image.jpg',name:'Will Smith'},status:'Pending',role:'Super admin',email:'mahmoudsmohammed24@gmail.com',date:'11:43 am-18jun,2024'}];
   
-  constructor(){
-    
+  constructor(private userManageServ:UserManagementsService){}
+
+  ngOnInit(): void {
+    this.userManageServ.getAllUsers().subscribe(res=>{
+      this.bodydata = res;
+    },err=>{
+      console.log(err,"from table");
+    })
   }
 
   getCount(e:number){
